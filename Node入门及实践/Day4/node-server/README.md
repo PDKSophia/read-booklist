624ad149312388c9cee016d6e96eeedfd3cc3d15
DK_Peng <1063137960@qq.com>
2019-01-04 11:49:28 +0800

    docs: 修改README.md

--- Node入门及实践/Day4/node-server/README.md
+++ Node入门及实践/Day4/node-server/README.md
@@ -1,3 +1,7 @@
+## 简介
+
+erek-market 后端代码，感觉代码架构很糟，对于跟我一样的 node 新手，凑合看看吧
+
 ## 常见问题
 
 - [How to allow CORS ?](https://stackoverflow.com/questions/7067966/how-to-allow-cors#answer-7069902)
--- Node入门及实践/Day4/node-server/app.js
+++ Node入门及实践/Day4/node-server/app.js
@@ -1,29 +1,24 @@
 /**
  * 模块依赖
  */
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var app = express();
-var routes = require('./routes');
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var app = express()
+var routes = require('./routes')
 
 var whitelist = [
-  'http://localhost:6969',
-  'http://www.pengdaokuan.cn/erek-market',
-  'http://www.pengdaokuan.cn',
-  'https://www.pengdaokuan.cn',
-  'http://www.pengdaokuan.cn/erek-market/#/'
-];
+  'http://localhost:6969' // 白名单
+]
 var corsOptions = {
   origin: function(origin, callback) {
-    console.log('@@@@@@@', origin);
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS'))
     }
   }
-};
+}
 
 /**
  * 中间件
@@ -32,26 +27,26 @@ app.use(
   bodyParser.urlencoded({
     extended: true
   })
-);
-app.use(bodyParser.json());
-app.use(cors({ credentials: true }));
-app.options('*', cors());
+)
+app.use(bodyParser.json())
+app.use(cors({ credentials: true }))
+app.options('*', cors())
 
-app.use('/', cors(corsOptions));
-routes(app);
+app.use('/', cors(corsOptions))
+routes(app)
 /**
  * 跨域
  */
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type'); //预检请求使用
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS'); //预检请求使用
-  next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type') //预检请求使用
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS') //预检请求使用
+  next()
+})
 /**
  * 端口
  */
-app.set('port', process.env.PORT || 2442);
+app.set('port', process.env.PORT || 2442)
 app.listen(app.get('port'), function() {
-  console.log('Express server is running at : ' + app.get('port'));
-});
+  console.log('Express server is running at : ' + app.get('port'))
+})
